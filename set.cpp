eda9b7ce71ec7dc73d44df7c8a25fa7b15ee1a2b
aniketgauba67 <85576196+aniketgauba67@users.noreply.github.com>
2024-01-25 19:37:46 -0500

    Create set.cpp

---
+++ set.cpp
@@ -0,0 +1,275 @@
+1 of 8,990
+(no subject)
+Inbox
+Hoang Ngo
+	
+Attachments6:47 PM (46 minutes ago)
+	
+to me
+
+ 2 Attachments  •  Scanned by Gmail
+	
+
+//========================================================
+// Set
+// Default constructor creating an empty set.
+// Parameters: None
+// Return Value: None
+//========================================================
+
+template <class T>
+		Set<T>::Set		( void )
+{
+	head = NULL;
+}
+
+//==============================================================
+// Set
+// Copy constructor to create a new set from an existing one.
+// Parameters: const Set<T> &s
+// Return Value: None
+//==============================================================
+
+template <class T>
+		Set<T>::Set		( const Set<T> &s )
+{
+	Node *pre = NULL;
+	Node *cur = s.head;
+	
+	while ( cur != NULL )
+	{
+		Node *newNode = new Node;
+		newNode->data = cur->data;
+		newNode->next = NULL;
+    	
+		if ( pre == NULL )
+		{
+			head = newNode;
+		}
+		else
+		{
+			pre->next = newNode;
+		}
+		pre = newNode;
+		cur = cur->next;
+	}
+}
+
+//==============================================================
+// ~Set
+// Destructor to clean up the memory of the set.
+// Parameters: None
+// Return Value: None
+//==============================================================
+
+template <class T>
+		Set<T>::~Set		( void )
+{
+	Node *cur = head;
+	Node *next = NULL;
+	
+	while (cur != NULL)
+	{
+	next = cur->next; 
+	delete cur; 
+	cur = next; 
+	}	
+	head = NULL;
+}
+
+//==============================================================
+// insert
+// Adds a new Elements at the head of the list.
+// Parameters: T item (item to be added)
+// Return Value: None
+//==============================================================
+
+template <class T>
+void		Set<T>::insert		( const T &value )
+{
+	Node *cur = head;
+	while ( cur != NULL )
+	{
+		if (cur->data == value)
+		{
+			return;
+		}
+		cur = cur->next;
+	}
+	Node *newNode = new Node;
+	newNode->data = value;
+	newNode->next = head;
+	head = newNode;
+}
+
+//==============================================================
+// remove
+// Removes an element x from the set s.
+// Parameters: 
+// Return Value: None
+//==============================================================
+
+template <class T>
+void		Set<T>::remove		( const T &value )
+{
+	if ( head == NULL )
+	{
+		return;
+	}
+	
+	if ( head->data == value)
+	{
+	Node *temp = head;
+	head = head->next;
+	delete temp;
+	return;
+	}
+	
+	Node *cur = head;
+	while (cur->next != NULL && cur->next->data != value)
+	{
+	cur = cur->next;
+	}
+	
+	if (cur->next != NULL)
+	{
+	Node *temp = cur->next;
+	cur->next = cur->next->next
+	delete temp;
+	}
+}
+
+//==============================================================
+// cardinality
+// Returns the cardinality of set s.
+// Parameters: None
+// Return Value: cardinality of set
+//==============================================================
+
+template <class T>
+int		Set<T>::cardinality	( void ) const
+{
+	int count = 0;
+	Node *cur = head;
+	
+	while ( cur != NULL)
+	{
+		count++;
+		cur=cur->next;
+	}
+	return count;
+}
+
+//==============================================================
+// empty
+// Returns true or false whether s is the empty set.
+// Parameters: None
+// Return Value: bool value
+//==============================================================
+
+template <class T>
+bool		Set<T>::empty		( void ) const
+{
+	return ( head = NULL );
+}
+
+//==============================================================
+// contains
+// Returns whwther an element x is contained in the set s.
+// Parameters: None
+// Return Value: bool value
+//==============================================================
+
+template <class T>
+bool		Set<T>::contain	( const T &value ) const
+{
+	Node *cur = head;
+	
+	while (cur != NULL)
+	{
+		if (cur->data = value)
+		{
+			return true;
+		}
+		cur = cur->next;
+	}
+	return false;
+}
+
+//==============================================================
+// operator== 
+//
+//
+//
+//==============================================================
+
+template <class T>
+bool		Set<T>::operator==	( const set<T> &s )
+{
+	if (this->cardinality != s.cardinality())
+	{
+		return false;
+	}
+	
+	Node *cur = head;
+	while (cur != NULL)
+	{
+		if (!s.contains(cur->data))
+		{
+			return false;
+		}
+		cur=cur->next
+	}	
+	return true;
+}
+
+//==============================================================
+// operator<= 
+//
+//
+//
+//==============================================================
+
+template <class T>
+bool		Set<T>::operator<=	( const set<T> &s )
+{
+	if (this->cardinality > s.cardinality())
+	{
+		return false;
+	}
+	
+	Node *cur = head;
+	while (cur != NULL)
+	{
+		if (!s.contains(cur->data))
+		{
+			return false;
+		}
+		cur=cur->next
+	}	
+	return true;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
